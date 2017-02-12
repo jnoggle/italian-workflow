d59b6c189ff37c2c9c3f13f9a4d870301a6058c9
jnoggle <justin.noggle@gmail.com>
2017-02-12 17:38:21 -0600

    Write new server api
    Write a server api that handles user login and registration. Replaces the old server api borrowed from Auth0

--- api/user-routes.js
+++ api/user-routes.js
@@ -1,88 +1,55 @@
 var express = require('express'),
-  _ = require('lodash'),
-  config = require('./config'),
-  jwt = require('jsonwebtoken');
+    _ = require('lodash'),
+    config = require('./config'),
+    jwt = require('jsonwebtoken');
 
 var app = module.exports = express.Router();
 
-// XXX: This should be a database of users :).
 var users = [{
-  id: 1,
-  username: 'admin',
-  password: 'admin'
+    id: 1,
+    username: 'admin',
+    password: 'admin'
 }];
 
 function createToken(user) {
-  return jwt.sign(_.omit(user, 'password'), config.secret, { expiresIn: 60 * 60 * 5 });
-}
-
-function getUserScheme(req) {
-
-  var username;
-  var type;
-  var userSearch = {};
-
-  // The POST contains a username and not an email
-  if (req.body.username) {
-    username = req.body.username;
-    type = 'username';
-    userSearch = { username: username };
-  }
-  // The POST contains an email and not an username
-  else if (req.body.email) {
-    username = req.body.email;
-    type = 'email';
-    userSearch = { email: username };
-  }
-
-  return {
-    username: username,
-    type: type,
-    userSearch: userSearch
-  }
+    return jwt.sign(_.omit(user, 'password'), config.secret, { expiresIn: 60 * 60 * 5 });
 }
 
 app.post('/users', function (req, res) {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).send("Please enter your username and password");
+    }
 
-  var userScheme = getUserScheme(req);
+    if (_.find(users, req.body.username)) {
+        return res.status(400).send("There is already a user with that name");
+    }
 
-  if (!userScheme.username || !req.body.password) {
-    return res.status(400).send("You must send the username and the password");
-  }
+    var user = _.pick(req.body, 'username', 'password');
+    user.id = _.max(users, 'id').id + 1;
 
-  if (_.find(users, userScheme.userSearch)) {
-    return res.status(400).send("A user with that username already exists");
-  }
+    users.push(user);
 
-  var profile = _.pick(req.body, userScheme.type, 'password', 'extra');
-  profile.id = _.max(users, 'id').id + 1;
-
-  users.push(profile);
-
-  res.status(201).send({
-    id_token: createToken(profile)
-  });
+    res.status(201).send({
+        id_token: createToken(user)
+    });
 });
 
 app.post('/sessions/create', function (req, res) {
+    if (!req.body.username || !req.body.password) {
+        return res.status(400).send("Please enter your username and password");
+    }
 
-  var userScheme = getUserScheme(req);
+    var user = _.find(users, { username: req.body.username });
 
-  if (!userScheme.username || !req.body.password) {
-    return res.status(400).send("You must send the username and the password");
-  }
+    if (!user) {
+        return res.status(401).send("The username or password doesn't match");
+    }
 
-  var user = _.find(users, userScheme.userSearch);
+    if (user.password !== req.body.password) {
+        return res.status(401).send("The username or password doesn't match");
+    }
 
-  if (!user) {
-    return res.status(401).send("The username or password don't match");
-  }
-
-  if (user.password !== req.body.password) {
-    return res.status(401).send("The username or password don't match");
-  }
-
-  res.status(201).send({
-    id_token: createToken(user)
-  });
+    res.status(201).send({
+        id_token: createToken(user)
+    });
 });
