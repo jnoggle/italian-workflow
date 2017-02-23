3339695f4bb03fcaec26d4fb8d69022281d78a25
jnoggle <justin.noggle@gmail.com>
2017-02-23 09:50:44 -0600

    Add gift certificates REST endpoint

---
+++ api/gift-certificate-routes.js
@@ -0,0 +1,48 @@
+var express = require('express'),
+    _ = require('lodash'),
+    config = require('./config'),
+    jwt = require('jsonwebtoken'),
+    mysql = require('mysql'),
+    bcrypt = require('bcrypt');
+
+var app = module.exports = express.Router();
+
+var conn = mysql.createConnection({
+    host: config.sql_host,
+    user: config.sql_user,
+    port: config.sql_port,
+    password: config.sql_password,
+    database: config.sql_database,
+});
+
+const saltRounds = 10;
+
+app.put('/gift-certificates/create', function (req, res) {
+    var id = req.body.id;
+    var amount = req.body.amount;
+    var sold_date = req.body.sold_date;
+    var seller_id = req.body.seller_id;
+    var redeemed_date = req.body.redeemed_date;
+
+    if (!id ||
+        !amount ||
+        !sold_date ||
+        !seller_id ||
+        !redeemed_date) {
+            return res.status(400).send("Please enter all fields");
+        }
+    
+    var sql = 'INSERT INTO GiftCertificates (id, amount, sold_date, seller_id) VALUES ('
+    + conn.escape(id) + ' '
+    + conn.escape(amount) + ' '
+    + conn.escape(sold_date) + ' '
+    + conn.escape(seller_id);
+
+    var query = conn.query(sql, function (err, results) {
+        if (err) {
+            console.log(err);
+            return res.status(400).send("Database error");
+        }
+        res.status.apply(201).send();
+    });
+});
