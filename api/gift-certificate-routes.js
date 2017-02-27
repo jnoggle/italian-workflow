8d3beb714dd5fdaba04e284599319c1fbc4ef608
Justin Noggle <justin.noggle@gmail.com>
2017-02-27 16:22:46 -0600

    Add endpoints to gc's

--- api/gift-certificate-routes.js
+++ api/gift-certificate-routes.js
@@ -20,23 +20,24 @@ const saltRounds = 10;
 app.post('/gift-certificates/create', function (req, res) {
     var id = req.body.id;
     var amount = req.body.amount;
+    var sale_price = req.body.sale_price;
     var sold_date = req.body.sold_date;
-    var seller_id = req.body.seller_id;
-    var redeemed_date = req.body.redeemed_date;
+    var issuer_id = req.body.issuer_id;
+    var memo = req.body.memo;
 
     if (!id ||
         !amount ||
+        !sale_price ||
         !sold_date ||
-        !seller_id ||
-        !redeemed_date) {
+        !issuer_id) {
         return res.status(400).send("Please enter all fields");
     }
 
-    var sql = 'INSERT INTO GiftCertificates (id, amount, sold_date, seller_id) VALUES ('
+    var sql = 'INSERT INTO GiftCertificates (id, amount, sold_date, issuer_id) VALUES ('
         + conn.escape(id) + ' '
         + conn.escape(amount) + ' '
         + conn.escape(sold_date) + ' '
-        + conn.escape(seller_id);
+        + conn.escape(issuer_id);
 
     var query = conn.query(sql, function (err, results) {
         if (err) {
@@ -48,5 +49,29 @@ app.post('/gift-certificates/create', function (req, res) {
 });
 
 app.get('/gift-certificates:id', function (req, res) {
+    var id = req.body.id;
 
+    if (!id) {
+        return res.status(400).send("Please enter an id");
+    }
+
+    var sql = 'SELECT * FROM GiftCertificates WHERE id = ' + conn.escape(id);
+    var query = conn.query(sql, function (err, results) {
+        if (err) {
+            console.log(err);
+            return res.status(400).send("Database error");
+        }
+        res.status(200).json.apply(JSON.stringify(results));
+    });
+});
+
+app.get('/gift-certificates', function (req, res) {
+    var sql = 'SELECT * FROM GiftCertificates';
+    var query = conn.query(sql, function (err, results) {
+        if (err) {
+            console.log(err);
+            return res.status(400).send("Database error");
+        }
+        res.status(200).json(JSON.stringify(users));
+    })
 })
