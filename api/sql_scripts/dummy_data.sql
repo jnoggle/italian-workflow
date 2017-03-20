1ebdcdf001749eb372338bb401caffd94619025f
jnoggle <justin.noggle@gmail.com>
2017-03-20 14:22:54 -0500

    Create dummy data scripts for gc's

---
+++ api/sql_scripts/dummy_data.sql
@@ -0,0 +1,4 @@
+USE italian-workflow;
+
+INSERT INTO GiftCertificates (amount, sale_price, date_sold, issuer_id, memo) VALUES (25.00, 25.00, "2017-2-25", 1, "Test this DB yeeeeeeee boy!");
+INSERT INTO GiftCertificates (amount, sale_price, date_sold, issuer_id, memo) VALUES (10.00, 00.00, "2017-1-20", 1, "DAAYMN");
