1628f48d18ab563b2ac080530c7868adf9245201
jnoggle <justin.noggle@gmail.com>
2017-03-29 21:27:57 -0500

    Add gc by date endpoints
    Fix bug where date wasn't taking into account the utc offset

--- api/gift-certificate-routes.js
+++ api/gift-certificate-routes.js
@@ -25,7 +25,7 @@ app.post('/gift-certificates', function (req, res) {
 
     var determine_id_from_token = 1;
     var issuer_id = determine_id_from_token;
-    var date_sold = new Date().toISOString().substring(0,10);
+    var date_sold = getDate();
 
     if (!amount ||
         !sale_price) {
@@ -63,7 +63,7 @@ app.get('/gift-certificates/:id', function (req, res) {
     var id = req.body.id;
 
     if (!id) {
-        return res.status(400).send("Please enter an id");
+        return res.status(400).send("Invalid request, expected id");
     }
 
     var sql = 'SELECT * FROM GiftCertificates WHERE gift_certificate_id = ' + conn.escape(id);
@@ -76,6 +76,43 @@ app.get('/gift-certificates/:id', function (req, res) {
     });
 });
 
+app.get('/gift-certificates/:date', function (req, res) {
+    var date = req.body.date;
+
+    if (!date) {
+        return res.status(400).send("Invalid request, expected date");
+    }
+
+    var sql = 'SELECT * FROM GiftCertificates WHERE date_sold = ' + conn.escape(date); _
+    var query = conn.query(sql, function (err, results) {
+        if (err) {
+            console.log(err);
+            return res.status(400).send("Database error");
+        }
+
+        res.status(200).json(results);
+    });
+});
+
+app.get('/gift-certificates/:dates', function (req, res) {
+    var start_date = req.body.start_date;
+    var end_date = req.body.end_date;
+
+    if (!start_date || !end_date) {
+        return res.status(400).send("Invalid request, expected start_date and end_date");
+    }
+
+    var sql = 'SELECT * FROM GiftCertificates WHERE date_sold BETWEEN ' + conn.escape(start_date) + ' AND ' + conn.escape(end_date);
+    var query = conn.query(sql, function (err, results) {
+        if (err) {
+            console.log(err);
+            return res.status(400).send("Database error");
+        }
+
+        res.status(200).json(results);
+    });
+});
+
 app.get('/gift-certificates', function (req, res) {
     var sql = 'SELECT * FROM GiftCertificates ORDER BY date_sold DESC, gift_certificate_id DESC';
     var query = conn.query(sql, function (err, results) {
@@ -83,8 +120,24 @@ app.get('/gift-certificates', function (req, res) {
             console.log(err);
             return res.status(400).send("Database error");
         }
-        
-        console.log(results);
+
         res.status(200).json(results);
     });
 });
+
+function getDate() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var month = pad(date.getMonth() + 1);
+    var day = pad(date.getDate());
+
+    return [year, month, day].join("-");
+}
+
+function pad(number) {
+    var r = String(number);
+    if (r.length === 1) {
+        r = '0' + r;
+    }
+    return r;
+}
